ca0b6d6e692d0a9056b73a1532318ff19ea13922
Richard Hosler <kaltaugh@gmail.com>
2022-02-02 18:50:45 +0000

    feat: Array test added

---
+++ test/Array.test.ts
@@ -0,0 +1,66 @@
+import { expect, use } from 'chai';
+import { Contract } from 'ethers';
+import { deployContract, MockProvider, solidity } from 'ethereum-waffle';
+import arrayContract from '../build/Array.json';
+
+use(solidity);
+
+describe("Array", () => {
+    const [wallet] = new MockProvider().getWallets();
+    let Array: Contract;
+
+    beforeEach(async () => {
+        Array = await deployContract(wallet, arrayContract);
+    });
+
+    it("Array is deployed on signer address", async () => {
+        expect(await Array.signer.getAddress()).to.equal(wallet.address);
+    });
+    it("fixed arrays initialise to default values", async () => {
+        expect(await Array.myFixedSizeArr(3)).to.equal(0);
+    });
+    it("reverts if you try to access an empty array", async () => {
+        await expect(Array.arr(0)).to.be.reverted;
+        await expect(Array.get(0)).to.be.reverted;
+    });
+    it("adds an element to the array with push", async () => {
+        await Array.push(42);
+        expect(await Array.get(0)).to.equal(42);
+    });
+    it("can create a memory only array", async () => {
+        const arr = await Array.examples();
+        console.log(arr);
+
+    })
+    describe("testing multiple values", async () => {
+        beforeEach(async () => {
+            await Array.push(4);
+            await Array.push(2);
+            await Array.push(42);
+        });
+        it("can return the full array", async () => {
+            const arr = await Array.getArr();
+            expect(arr[0]).to.equal(4);
+            expect(arr[1]).to.equal(2);
+            expect(arr[2]).to.equal(42);
+        });
+        it("pop removes the last element", async () => {
+            await Array.pop();
+            expect(await Array.getLength()).to.equal(2);
+            const arr = await Array.getArr();
+            expect(arr[0]).to.equal(4);
+            expect(arr[1]).to.equal(2);
+        });
+        it("length is returned correctly", async () => {
+            expect(await Array.getLength()).to.equal(3);
+        });
+        it("can remove an element leaving default value", async () => {
+            await Array.remove(1);
+            const arr = await Array.getArr();
+            expect(arr[0]).to.equal(4);
+            expect(arr[1]).to.equal(0);
+            expect(arr[2]).to.equal(42);
+        });
+    });
+
+});
