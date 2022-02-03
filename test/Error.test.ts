e8ec9745cb55420f8c50de4c320f6c729aeeded3
Chrykal <kaltaugh@gmail.com>
2022-02-03 19:11:21 +0000

    feat: Error test finished

--- test/Error.test.ts
+++ test/Error.test.ts
@@ -12,15 +12,25 @@ describe("Error", () => {
     beforeEach(async () => {
         Error = await deployContract(wallet, ErrorContract);
     });
-
     it("Error is deployed on signer address", async () => {
         expect(await Error.signer.getAddress()).to.equal(wallet.address);
     });
     it("require reverts correctly", async () => {
-        expect(await Error.testRequire(5)).to.be.called;
-        await expect(Error.testRequire(11)).to.be.reverted;
+        await Error.testRequire(11);
+        expect("testRequire").to.be.calledOnContract(Error);
+        await expect(Error.testRequire(5)).to.be.reverted;
     });
-    it("revert reverts correctlt");
-    it("assert reverts correctly");
-    it("custiom error is thrown");
+    it("revert reverts correctly", async () => {
+        await Error.testRequire(11);
+        expect("testRequire").to.be.calledOnContract(Error);
+        await expect(Error.testRequire(5)).to.be.reverted;
+    });
+    it("assert reverts correctly", async () => {
+        await expect(Error.testAssert()).to.not.be.reverted;
+    });
+    it("custom error is thrown", async () => {
+        await expect(Error.testCustomError(0)).to.not.be.reverted;
+        await expect(Error.testCustomError(42)).to.be.reverted;
+
+    });
 });
