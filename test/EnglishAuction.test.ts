4c1d5bcdbf2351e741d9e607b316278b89364e8a
Richard Hosler <kaltaugh@gmail.com>
2022-03-01 16:41:08 +0000

    English Auction started

---
+++ test/EnglishAuction.test.ts
@@ -0,0 +1,32 @@
+import { expect, use } from 'chai';
+import { Contract } from 'ethers';
+import { deployContract, MockProvider, solidity } from 'ethereum-waffle';
+import EnglishAuctionContract from '../build/EnglishAuction.json';
+import ERC721Contract from '../build/ERC721.json';
+
+
+use(solidity);
+
+describe("English Auction", () => {
+    const [owner, buyer] = new MockProvider().getWallets();
+    let EnglishAuction: Contract;
+    let ERC721: Contract;
+
+    before(async () => {
+        ERC721 = await deployContract(owner, ERC721Contract);
+        await ERC721.connect(owner).mint(owner.address, 42);
+        EnglishAuction = await deployContract(owner, EnglishAuctionContract, [ERC721.address, 42n, 500n]);
+        await ERC721.setApprovalForAll(EnglishAuction.address, true);
+    });
+
+    it("English Auction is deployed on signer address", async () => {
+        expect(await EnglishAuction.signer.getAddress()).to.equal(owner.address);
+    });
+    it("has token to sell", async () => {
+        expect(await ERC721.ownerOf(42n)).to.equal(owner.address);
+    })
+    it("can start auction", async () => {
+        expect(await EnglishAuction.connect(owner).start()).to.emit(EnglishAuction, "Start");
+    });
+
+});
